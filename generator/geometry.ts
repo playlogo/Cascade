5c21089be89bb8c2316b7ce2ba39303a6370445c
playlogo <playlogo@yahoo.com>
2024-11-10 16:26:42 +0100

    Optimized output page for codepen

--- generator/geometry.ts
+++ generator/geometry.ts
@@ -2,6 +2,7 @@ import { Keyframe } from "./project.ts";
 import { Vector } from "./vector.ts";
 
 const FRAME_RATE = 60;
+const MAX_DECIMALS = 2;
 
 export abstract class Shape {
 	abstract toHtml(): string;
@@ -56,98 +57,126 @@ export class Box extends Shape {
 
 	override toHtml(): string {
 		return `
-            <div class="box_${this.index}">
-                <div class="box_face_${this.index} box_face_${this.index}_front"></div>
-                <div class="box_face_${this.index} box_face_${this.index}_back"></div>
-                <div class="box_face_${this.index} box_face_${this.index}_right"></div>
-                <div class="box_face_${this.index} box_face_${this.index}_left"></div>
-                <div class="box_face_${this.index} box_face_${this.index}_top"></div>
-                <div class="box_face_${this.index} box_face_${this.index}_bottom"></div>
+            <div class="b_${this.index}">
+                <div class="b_f_${this.index} b_f_${this.index}_f"></div>
+                <div class="b_f_${this.index} b_f_${this.index}_ba"></div>
+                <div class="b_f_${this.index} b_f_${this.index}_r"></div>
+                <div class="b_f_${this.index} b_f_${this.index}_l"></div>
+                <div class="b_f_${this.index} b_f_${this.index}_t"></div>
+                <div class="b_f_${this.index} b_f_${this.index}_bo"></div>
             </div>
         `;
 	}
 
 	override toCss(camera: Camera): string {
+		let includedKeyframes = new Set<number>();
 		const keyframes = this.keyframes
+			.filter((keyframe) => {
+				const frame = Math.floor((keyframe.frame / this.totalFrames) * 100);
+				if (includedKeyframes.has(frame)) {
+					return false;
+				}
+
+				includedKeyframes.add(frame);
+				includedKeyframes.add(frame + 1);
+
+				return true;
+			})
 			.map((keyframe) => {
 				return `
                 ${Math.floor((keyframe.frame / this.totalFrames) * 100)}% {
-                    transform: translateX(calc(50vw -  ${
-						keyframe.loc.x + keyframe.scale.x / 2 - camera.pos.x
-					}px)) 
-                          translateY(calc(50vh - ${keyframe.loc.y + keyframe.scale.y / 2 - camera.pos.y}px)) 
-                          translateZ(${keyframe.loc.z + keyframe.scale.z / 2 - camera.pos.z}px) 
-                          rotate3D(${keyframe.rot[1]}, ${keyframe.rot[2]}, ${keyframe.rot[3]}, ${
-					keyframe.rot[0]
-				}deg);
+                    transform: translateX(calc(50vw -  ${(
+						keyframe.loc.x +
+						keyframe.scale.x / 2 -
+						camera.pos.x
+					).toFixed(MAX_DECIMALS)}px)) 
+                          translateY(calc(50vh - ${(
+								keyframe.loc.y +
+								keyframe.scale.y / 2 -
+								camera.pos.y
+							).toFixed(MAX_DECIMALS)}px)) 
+                          translateZ(${(keyframe.loc.z + keyframe.scale.z / 2 - camera.pos.z).toFixed(
+								MAX_DECIMALS
+							)}px) 
+                          rotate3D(${keyframe.rot[1].toFixed(MAX_DECIMALS)}, ${keyframe.rot[2].toFixed(
+					MAX_DECIMALS
+				)}, ${keyframe.rot[3].toFixed(MAX_DECIMALS)}, ${keyframe.rot[0].toFixed(MAX_DECIMALS)}deg);
                 }
             `;
 			})
 			.join("\n");
 
 		return `
-            .box_${this.index} {
-                width: ${this.size.x}px;
-                height: ${this.size.y}px;
+            .b_${this.index} {
+                width: ${this.size.x.toFixed(MAX_DECIMALS)}px;
+                height: ${this.size.y.toFixed(MAX_DECIMALS)}px;
                 position: absolute;
                 transform-style: preserve-3d;
-                transform: translateX(calc(50vw -  ${this.pos.x + this.size.x / 2 - camera.pos.x}px)) 
-                          translateY(calc(50vh - ${this.pos.y + this.size.y / 2 - camera.pos.y}px)) 
-                          translateZ(${this.pos.z + this.size.z / 2 - camera.pos.z}px) 
-                          rotate3D(${this.rot[1]}, ${this.rot[2]}, ${this.rot[3]}, ${this.rot[0]}deg);
+                transform: translateX(calc(50vw -  ${(this.pos.x + this.size.x / 2 - camera.pos.x).toFixed(
+					MAX_DECIMALS
+				)}px)) 
+                          translateY(calc(50vh - ${(this.pos.y + this.size.y / 2 - camera.pos.y).toFixed(
+								MAX_DECIMALS
+							)}px)) 
+                          translateZ(${(this.pos.z + this.size.z / 2 - camera.pos.z).toFixed(
+								MAX_DECIMALS
+							)}px) 
+                          rotate3D(${this.rot[1].toFixed(MAX_DECIMALS)}, ${this.rot[2].toFixed(
+			MAX_DECIMALS
+		)}, ${this.rot[3].toFixed(MAX_DECIMALS)}, ${this.rot[0].toFixed(MAX_DECIMALS)}deg);
                 animation-duration: ${this.totalFrames / FRAME_RATE}s;
                 animation-iteration-count: infinite;
                 animation-direction: normal;
-                animation-name: box_${this.index}_animation;
+                animation-name: b_${this.index}_a;
             }
 
-            @keyframes box_${this.index}_animation {
+            @keyframes b_${this.index}_a {
                 ${keyframes}
             }
 
-            .box_face_${this.index} {
+            .b_f_${this.index} {
                 position: absolute;
                 border: 1px solid black;
             }
 
-            .box_face_${this.index}_front,
-            .box_face_${this.index}_back {
-                width: ${this.size.x}px;
-                height: ${this.size.y}px;
+            .b_f_${this.index}_f,
+            .b_f_${this.index}_ba {
+                width: ${this.size.x.toFixed(MAX_DECIMALS)}px;
+                height: ${this.size.y.toFixed(MAX_DECIMALS)}px;
             }
 
-            .box_face_${this.index}_right,
-            .box_face_${this.index}_left {
-                width: ${this.size.z}px;
-                height: ${this.size.y}px;
-                left: ${this.size.x / 2 - this.size.z / 2}px;
+            .b_f_${this.index}_r,
+            .b_f_${this.index}_l {
+                width: ${this.size.z.toFixed(MAX_DECIMALS)}px;
+                height: ${this.size.y.toFixed(MAX_DECIMALS)}px;
+                left: ${(this.size.x / 2 - this.size.z / 2).toFixed(MAX_DECIMALS)}px;
             }
 
-            .box_face_${this.index}_top,
-            .box_face_${this.index}_bottom {
-                width: ${this.size.x}px;
-                height: ${this.size.z}px;
-                top: ${this.size.y / 2 - this.size.z / 2}px;
+            .b_f_${this.index}_t,
+            .b_f_${this.index}_bo {
+                width: ${this.size.x.toFixed(MAX_DECIMALS)}px;
+                height: ${this.size.z.toFixed(MAX_DECIMALS)}px;
+                top: ${(this.size.y / 2 - this.size.z / 2).toFixed(MAX_DECIMALS)}px;
             }
 
-            .box_face_${this.index}_front  { background: ${
+            .b_f_${this.index}_f  { background: ${
 			this.faceColors[0]
-		}; transform: rotateY(  0deg) translateZ(${this.size.z / 2}px); }
-            .box_face_${this.index}_right  { background: ${
+		}; transform: rotateY(  0deg) translateZ(${(this.size.z / 2).toFixed(MAX_DECIMALS)}px); }
+            .b_f_${this.index}_r  { background: ${
 			this.faceColors[3]
-		}; transform: rotateY( 90deg) translateZ(${this.size.x / 2}px); }
-            .box_face_${this.index}_back   { background: ${
+		}; transform: rotateY( 90deg) translateZ(${(this.size.x / 2).toFixed(MAX_DECIMALS)}px); }
+            .b_f_${this.index}_ba   { background: ${
 			this.faceColors[2]
-		}; transform: rotateY(180deg) translateZ(${this.size.z / 2}px); }
-            .box_face_${this.index}_left   { background: ${
+		}; transform: rotateY(180deg) translateZ(${(this.size.z / 2).toFixed(MAX_DECIMALS)}px); }
+            .b_f_${this.index}_l   { background: ${
 			this.faceColors[1]
-		}; transform: rotateY(-90deg) translateZ(${this.size.x / 2}px); }
-            .box_face_${this.index}_top    { background: ${
+		}; transform: rotateY(-90deg) translateZ(${(this.size.x / 2).toFixed(MAX_DECIMALS)}px); }
+            .b_f_${this.index}_t    { background: ${
 			this.faceColors[4]
-		}; transform: rotateX( 90deg) translateZ(${this.size.y / 2}px); }
-            .box_face_${this.index}_bottom { background: ${
+		}; transform: rotateX( 90deg) translateZ(${(this.size.y / 2).toFixed(MAX_DECIMALS)}px); }
+            .b_f_${this.index}_bo { background: ${
 			this.faceColors[5]
-		}; transform: rotateX(-90deg) translateZ(${this.size.y / 2}px); }
+		}; transform: rotateX(-90deg) translateZ(${(this.size.y / 2).toFixed(MAX_DECIMALS)}px); }
 
         `;
 	}
